0e9e4a8582a18e3744e52d4db0d46da0f1ecedf6
Ahmad Ali <ahmad.ali@codeninjaconsulting.com>
2024-10-04 08:34:08 +0500

    Initial commit

--- index.ts
+++ index.ts
@@ -3,10 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
-});
+const userRoutes = require('./routes/userRoutes');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use routes
+app.use('/users', userRoutes);
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
